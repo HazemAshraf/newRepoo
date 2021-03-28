2f48c9dfc3e5cdc01d83f068a020539c1518474a
HazemAshraf <hazem.ashraf.mongd@hotmail.com>
2021-03-28 14:21:59 +0200

    qwedw2erf

---
+++ src/MyMain.java
@@ -0,0 +1,6 @@
+public class MyMain {
+    public static void main(String[] args) {
+        String x = "Hazem";
+        if(x.equals("Hazem")) System.out.println(" this is Hazem user");
+       else  System.out.println("Hello world");
+    }}
